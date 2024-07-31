4dafac1f8060a594ee0b405a379cf5eaaa0d6b21
Gareth Somerville <gareth.somerville@infinityworks.com>
2024-07-31 10:57:34 +0100

    Use auto_enable_organization_members to enrol existing accounts

--- enable-gd/modules/guardduty-baseline/main.tf
+++ enable-gd/modules/guardduty-baseline/main.tf
@@ -15,25 +15,15 @@
 #  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 #  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-locals {
-  mem_accounts = var.gd_my_org.accounts
-  deleg_admin  = var.gd_delegated_admin_acc_id
-  temp = [
-    for x in local.mem_accounts :
-    x if x.id != local.deleg_admin && x.status == "ACTIVE"
-  ]
-}
-
 # GuardDuty Detector in the Delegated admin account
 resource "aws_guardduty_detector" "MyDetector" {
-  provider   = aws.dst
-  depends_on = [var.gd_my_org]
+  provider = aws.dst
 
   count = var.enabled ? 1 : 0
 
   enable                       = true
   finding_publishing_frequency = var.gd_finding_publishing_frequency
-  
+
   # Additional setting to turn on S3 Protection
   datasources {
     s3_logs {
@@ -61,35 +51,14 @@ resource "aws_guardduty_organization_configuration" "MyGDOrg" {
   depends_on = [aws_guardduty_organization_admin_account.MyGDOrgDelegatedAdmin]
   count      = var.enabled ? 1 : 0
 
-  auto_enable = true
-  detector_id = aws_guardduty_detector.MyDetector[0].id
-  
+  auto_enable_organization_members = "ALL"
+  detector_id                      = aws_guardduty_detector.MyDetector[0].id
+
   # Additional setting to turn on S3 Protection
   datasources {
     s3_logs {
       auto_enable = true
     }
-  }
-}
-
-# GuardDuty members in the Delegated admin account
-resource "aws_guardduty_member" "members" {
-  provider   = aws.dst
-  depends_on = [aws_guardduty_organization_configuration.MyGDOrg]
-
-  count = var.enabled ? length(local.temp) : 0
-
-  detector_id = aws_guardduty_detector.MyDetector[0].id
-  invite      = true
-
-  account_id                 = local.temp[count.index].id
-  disable_email_notification = true
-  email                      = local.temp[count.index].email
-
-  lifecycle {
-    ignore_changes = [
-      email
-    ]
   }
 }
 
